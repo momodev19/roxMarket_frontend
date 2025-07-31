1459ccf9308899dcef404310d50d5392e7d446f8
momodev19 <momodev19@gmail.com>
2025-07-31 23:50:11 +0000

    fix searchParams

--- src/app/items/prices/page.tsx
+++ src/app/items/prices/page.tsx
@@ -2,11 +2,10 @@ import GenericTable from "@/app/components/tables.tsx/GenericTable";
 import { getItemsWithPrice } from "@/lib/api/items";
 import { ItemTypeId } from "@/types/Items";
 
-export default async function ItemsPage({
-  searchParams,
-}: {
-  searchParams: ItemTypeId;
+export default async function ItemsPage(props: {
+  searchParams: Promise<ItemTypeId>;
 }) {
+  const searchParams = await props.searchParams;
   const items = await getItemsWithPrice(searchParams.typeId);
 
   return <GenericTable data={items} />;
