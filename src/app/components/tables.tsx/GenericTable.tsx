a9d26b82cc115a66a5b46fbf5a2a9c7415a66e7a
momodev19 <momodev19@gmail.com>
2025-08-05 02:34:41 +0000

    added filter, search, sort function

--- src/app/components/tables.tsx/GenericTable.tsx
+++ src/app/components/tables.tsx/GenericTable.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Table, Text } from "@mantine/core";
-import React, { useMemo } from "react";
+import { Table, Text, TextInput } from "@mantine/core";
+import React, { useMemo, useState } from "react";
+import { useDebouncedValue } from "@mantine/hooks";
 
 type RowValue = string | number | null | undefined;
 
@@ -20,6 +21,7 @@ type GenericTableProps = {
   columns?: Column[];
   renderRows?: (rows: Row[]) => React.ReactNode;
   tableOptions?: React.ComponentProps<typeof Table>;
+  showSearch?: boolean;
 };
 
 function displayNone(val: RowValue) {
@@ -35,6 +37,7 @@ export default function GenericTable({
     striped: true,
     withTableBorder: true,
   },
+  showSearch = true,
 }: GenericTableProps) {
   // Get columns from data if not provided
   const resolvedColumns = useMemo(() => {
@@ -42,6 +45,66 @@ export default function GenericTable({
     if (data?.length === 0) return [];
     return Object.keys(data[0]).map((key) => ({ key, label: key }));
   }, [columns, data]);
+
+  const [search, setSearch] = useState("");
+  const [sortBy, setSortBy] = useState<keyof Row>("id");
+  const [reverseSortDirection, setReverseSortDirection] = useState(false);
+  const [debouncedSearch] = useDebouncedValue(search, 500);
+
+  /**
+   * Start of sorting logic
+   */
+  const setSorting = (field: keyof Row) => {
+    if (sortBy === field) {
+      setReverseSortDirection(!reverseSortDirection);
+    } else {
+      setSortBy(field);
+      setReverseSortDirection(false);
+    }
+  };
+
+  const sortedData = useMemo(() => {
+    if (!sortBy) return data;
+
+    const sorted = [...data].sort((a, b) => {
+      const aVal = a[sortBy];
+      const bVal = b[sortBy];
+
+      if (aVal === undefined || aVal === null) return 1;
+      if (bVal === undefined || bVal === null) return -1;
+
+      if (aVal < bVal) return reverseSortDirection ? 1 : -1;
+      if (aVal > bVal) return reverseSortDirection ? -1 : 1;
+      return 0;
+    });
+
+    return sorted;
+  }, [data, sortBy, reverseSortDirection]);
+
+  /**
+   * End of sorting logic
+   */
+
+  /**
+   * Start of filtering logic
+   */
+  const filteredData = useMemo(() => {
+    if (!debouncedSearch) return sortedData;
+
+    const lowerDebouncedSearch = debouncedSearch.toLowerCase();
+
+    return sortedData.filter((item) =>
+      resolvedColumns.some((column) =>
+        item[column.key]
+          ?.toString()
+          .toLowerCase()
+          .includes(lowerDebouncedSearch)
+      )
+    );
+  }, [debouncedSearch, resolvedColumns, sortedData]);
+  /**
+   * End of filtering logic
+   */
 
   if (data?.length === 0) {
     return (
@@ -52,27 +115,61 @@ export default function GenericTable({
   }
 
   return (
-    <Table {...tableOptions}>
-      <Table.Thead>
-        <Table.Tr>
-          {resolvedColumns.map((column) => (
-            <Table.Th key={column.key}>{column.label}</Table.Th>
-          ))}
-        </Table.Tr>
-      </Table.Thead>
-      <Table.Tbody>
-        {renderRows
-          ? renderRows(data)
-          : data.map((row) => (
-              <Table.Tr key={row.id}>
-                {resolvedColumns.map((column, index) => (
-                  <Table.Td key={`${row.id}-${index}`}>
-                    {displayNone(row[column.key])}
-                  </Table.Td>
-                ))}
-              </Table.Tr>
+    <>
+      {showSearch && (
+        <TextInput
+          placeholder="Search..."
+          mb="sm"
+          value={search}
+          onChange={(event) => setSearch(event.currentTarget.value)}
+        />
+      )}
+      <Table stickyHeader stickyHeaderOffset={58} {...tableOptions}>
+        <Table.Thead>
+          <Table.Tr>
+            {resolvedColumns.map((column) => (
+              <Table.Th
+                tabIndex={0}
+                key={column.key}
+                style={{ cursor: "pointer" }}
+                onClick={() => setSorting(column.key)}
+                onKeyDown={(event) => {
+                  if (event.key === "Enter") {
+                    setSorting(column.key);
+                  }
+                }}
+                aria-sort={
+                  sortBy === column.key
+                    ? reverseSortDirection
+                      ? "descending"
+                      : "ascending"
+                    : "none"
+                }
+              >
+                {column.label}{" "}
+                {sortBy === column.key
+                  ? reverseSortDirection
+                    ? "▼"
+                    : "▲"
+                  : ""}
+              </Table.Th>
             ))}
-      </Table.Tbody>
-    </Table>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {renderRows
+            ? renderRows(filteredData)
+            : filteredData.map((row) => (
+                <Table.Tr key={row.id}>
+                  {resolvedColumns.map((column, index) => (
+                    <Table.Td key={`${row.id}-${index}`}>
+                      {displayNone(row[column.key])}
+                    </Table.Td>
+                  ))}
+                </Table.Tr>
+              ))}
+        </Table.Tbody>
+      </Table>
+    </>
   );
 }
